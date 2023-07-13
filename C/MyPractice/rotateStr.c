0ed5a044aad88b2e3ff046e23fb4f35e8e316318
CyberAakash <aakashvnth@gmail.com>
2023-07-13 21:50:53 +0530

    Still not getting correct answer

--- C/MyPractice/rotateStr.c
+++ C/MyPractice/rotateStr.c
@@ -1,30 +1,53 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+void rotate_right(char *str) {
+    int len = strlen(str);
+    char temp = str[len - 1];
+    for (int i = len - 1; i > 0; i--) {
+        str[i] = str[i - 1];
+    }
+    str[0] = temp;
+}
+
+void rotate_left(char *str) {
+    int len = strlen(str);
+    char temp1 = str[0];
+    char temp2 = str[1];
+    for (int i = 0; i < len - 2; i++) {
+        str[i] = str[i + 2];
+    }
+    str[len - 2] = temp1;
+    str[len - 1] = temp2;
+}
+
+int sum_of_squares(int num) {
+    int sum = 0;
+    while (num > 0) {
+        int digit = num % 10;
+        sum += digit * digit;
+        num /= 10;
+    }
+    return sum;
+}
 
 int main() {
-  char str[100], num[10];
-  int sum, i, rotate;
-
-  while (scanf("%s:%s", str, num) != EOF) {
-    sum = 0;
-    for (i = 0; i < strlen(num); i++) {
-      sum += (num[i] - '0') * (num[i] - '0');
+    char input[100];
+    fgets(input, 100, stdin);
+    char *token = strtok(input, ", ");
+    while (token != NULL) {
+        char *str = strtok(token, ":");
+        int num = atoi(strtok(NULL, ":"));
+        int sum = sum_of_squares(num);
+        if (sum % 2 == 0) {
+            rotate_right(str);
+        } else {
+            rotate_left(str);
+            rotate_left(str);
+        }
+        printf("%s ", str);
+        token = strtok(NULL, ", ");
     }
-
-    if (sum % 2 == 0) {
-      rotate = 1;
-    } else {
-      rotate = 2;
-    }
-
-    str[rotate] = str[0];
-    for (i = 1; i < strlen(str); i++) {
-      str[i - rotate] = str[i];
-    }
-    str[strlen(str) - rotate] = '\0';
-
-    printf("%s\n", str);
-  }
-
-  return 0;
+    return 0;
 }
--- C/MyPractice/rotateString.c
+++ C/MyPractice/rotateString.c
@@ -10,17 +10,68 @@
 // 1*1+2*2+4*4+6*6=85 which is odd so rotate string left by 2 so “ghftd” will be “ftdgh”
 
 
-// void rotateLeft2(char* start, char* end, int l) {
-//     char out[l+1];
-//     out[0] = *end;
-//     int j = 1;
-//     for(int i = 1; i < l-1; i++) {
-//         out[j++] = *start++;
-//     }
-//     out[j] = '\0';
 
-//     printf("Output : %s", out);
+
+
+// #include <stdio.h>
+// #include <string.h>
+// #include <stdlib.h>
+// #define MAX 100
+
+// // void rotateLeft2(char* start, char* end, int l) {
+
+// // }
+
+// // void rotateRight1(char* start, char* end, int l) {
+
+// // }
+
+// int splitColon(int start,int end, char* c) {
+//     int i;
+//     for(i = start; i <= end; i++) {
+//         if(c[i] == ':') {
+//             break;
+//         }
+//     }
+//     return i;
 // }
+
+// int main() {
+//     char str[MAX];\
+//     printf("Enter the  String :  ");
+//     fgets(str, sizeof(str), stdin);
+//     printf("\nThe given String is : %s", str);
+//     int len = strlen(str)-1 ; // to remove newline character from end
+//     printf("\nLength of string : %d", len);
+
+//     int start = 0,end;
+//     int i;
+
+//     for(i = 0; i <= len; i++) {
+//         if(str[i] == '\0' || str[i] == ',') {
+//             end = i-1;
+//             int x = splitColon(start, end, str);
+//             printf("Colon : %d", x);
+//             int sum = 0;
+//             for(int j = x+1; j <= end; j++) {
+//                 sum += (str[j] - '0')*(str[j] - '0');
+//             }
+//             if(sum % 2 == 0) {
+//                 printf("\nRightShift");
+//             } else {
+//                 printf("\nLeftShift");
+//             }
+
+//             start = i+1;
+//         }
+//     }
+        
+
+
+
+//     return 0;
+// }
+
 
 
 #include <stdio.h>
@@ -28,67 +79,82 @@
 #include <stdlib.h>
 #define MAX 100
 
-char str[MAX];
-
-void rotateRight1(char* start, char* end, int l) {
-    char out[l+1];
-    out[0] = *end;
-    int j = 1;
-    for(int i = 1; i < l-1; i++) {
-        out[j++] = *start++;
+void rotateLeft2(char* sub_str) {
+    int l = strlen(sub_str);
+    char out[l];
+    char temp1 = sub_str[0];
+    char temp2 = sub_str[1];
+    for (int i = 0; i < l - 2; i++) {
+        out[i] = sub_str[i + 2];
     }
-    out[j] = '\0';
+    out[-1] = temp1;
+    out[0] = temp2;
 
-    printf("Output : %s", out);
+    printf("\n%s ",out);
 }
 
+void rotateRight1(char* sub_str) {
+    int l = strlen(sub_str);
+    char out[l];
+    char temp = sub_str[0];
+    for (int i = l - 1; i > 0; i--) {
+        out[i] = sub_str[i - 1];
+    }
+    out[0] = temp;
 
+    printf("\n%s ",out);
+}
 
-void split(int start,int end) {
-    int s, e;
-    int i = start; 
-    while(str[i] == ':') {
-        s = i;
-        e = i -1;
-        int l = e - s + 1;
-        int sum = 0;
-        for(int j = i+1; j <= end; j++) {
-            sum += str[i] - '0';
+int splitColon(int end, char* c) {
+    int i;
+    for(i = 0; i <= end; i++) {
+        if(c[i] == ':') {
+            break;
         }
-        if(sum % 2 == 0) {
-            rotateRight1(&str[s], &str[e], l);
-        } 
-        // else {
-        //     rotateLeft2(&str[s], &str[e], l);
-        // }
-        i++;
     }
+    return i;
 }
-
 
 int main() {
-
-    printf("Enter the  String :  ");
+    char str[MAX];
+    printf("Enter the String: ");
     fgets(str, sizeof(str), stdin);
-    printf("\nThe given String is : %s", str);
-    int len = strlen(str)-1 ; // to remove newline character from end
-    printf("\nLength of string : %d", len);
+    printf("\nThe given String is: %s", str);
+    int len = strlen(str) - 1; // to remove newline character from end
+    printf("\nLength of string: %d", len);
 
-    int i = 0, j = 0;
-    while(str[i] != '\0') {
+    char* token = strtok(str, ",");
+    while(token != NULL) {
+        int l = strlen(token);
+        printf("\n%s", token);
+        char* sub_token = strtok(token, ":");
+        int cnt = 1;
+        char sub_str[l];
+        while(sub_token != NULL) {
+            int sub_len = strlen(sub_token);
+            int sum = 0;
+            if(cnt == 1) {
+                strcpy(sub_str,sub_token);
+                cnt++;
+            } else {
+                for(int i = 0; i < sub_len; i++) {
+                    sum += (sub_token[i] - '0')*(sub_token[i] - '0');
+                }
+            }
+            if(sum % 2 == 0 && sum != 0) {
+                printf("\nRightShift");
+                rotateRight1(sub_str);
+            } else if(sum % 2 != 0 && sum != 0) {
+                printf("\nLeftShift");
+                rotateRight1(sub_str);
+            }
 
-        int start, end;
-        if(str[i] == ',') {
-            start = j;
-            end = i-1;
-            j = i+1;
-            split(start,end);
+            sub_token = strtok(NULL, ",");
         }
 
-        i++;
+        token = strtok(NULL, ",");
     }
 
-
-
+    
     return 0;
 }
